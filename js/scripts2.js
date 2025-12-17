55aaff2f5bdffadb6b55f65c82f2a81d889c29a6
kachka16 <lirodaki345@gmail.com>
2025-12-17 23:15:05 +0200

    Завантаження усіх файлів

---
+++ js/scripts2.js
@@ -0,0 +1,94 @@
+ const cartWrapper = document.querySelector('.listCart');
+
+ window.addEventListener('click', function (event) {
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+        const counterWrapper = event.target.closest('.item-counter');
+         if(!counterWrapper)return;
+        const counter = counterWrapper.querySelector('[data-counter]');
+        if(event.target.dataset.action==='plus'){
+            counter.innerText=++counter.innerText;
+        }
+        if(event.target.dataset.action==='minus' && counter.innerText > 1){
+            counter.innerText=--counter.innerText;
+        }
+    }
+        if(event.target.closest('.cancle-img')){
+           event.target.closest('.card').remove();
+           calcCartPrice1();
+           toggleCartStatus();
+           calculateNumber();
+        }
+        
+        if (event.target.closest('[data-cart]')) {
+            const card = event.target.closest('.card');
+            const productInfo = {
+                id: card.dataset.id,
+                imgSrc: card.querySelector('.card-image').getAttribute('src'),
+                title: card.querySelector('.name-item').innerText,
+                price: parseInt(card.querySelector('.item-price').innerText),
+                counter: parseInt(card.querySelector('[data-counter]').innerText),
+            };
+            const totalPrice = productInfo.price* productInfo.counter;
+            const cartItemHTML = `<div class = "card" data-id= "${productInfo.id}" data-total="${totalPrice}">
+                <img src= "${productInfo.imgSrc}">
+                <h3 class =  "name-item">${productInfo.title}</h3>
+                <div class="item-price">${totalPrice}грн.</div>
+                <div class ="items_current" data-counter>${productInfo.counter}</div>
+                <img src ="images/cancle.png" class = "cancle-img" data-cancle>
+         </div>`;
+           cartWrapper.insertAdjacentHTML('beforeend',cartItemHTML);
+           calcCartPrice1();
+           toggleCartStatus();
+           calculateNumber();
+        }
+    });
+
+  const iconCart = document.querySelector('.cart-icon');
+  const btnClose = document.querySelector('.close');
+  const body = document.body;
+
+    iconCart.addEventListener('click',()=>{
+        body.classList.toggle('showCart');
+    });
+
+    btnClose.addEventListener('click',()=>{
+        body.classList.remove('showCart');
+    });
+
+    const iconUser = document.querySelector('.img-user');
+    const btnCloseUser = document.querySelector('.close');
+
+    iconUser.addEventListener('click',()=>{
+        body.classList.toggle('showUser');
+    });
+
+    btnCloseUser.addEventListener('click',()=>{
+        body.classList.remove('showUser');
+    });
+
+    function calcCartPrice1(){
+    const priceElements = cartWrapper.querySelectorAll('.card');
+    const totalPriceEl = document.querySelector('.total-price');
+    let priceTotal=0;
+    priceElements.forEach(function(item){
+        priceTotal+=parseInt(item.dataset.total);
+    });
+    totalPriceEl.textContent = priceTotal;
+}
+
+function toggleCartStatus(){
+    const cartEmptyBadge = document.querySelector('[data-cart-empty]');
+     const cartItems = cartWrapper.querySelectorAll('.card');
+    if(cartItems.length>0){
+        cartEmptyBadge.classList.add('none')
+    }
+    else{
+        cartEmptyBadge.classList.remove('none');
+    }
+}
+
+function calculateNumber(){
+    const cartItems = cartWrapper.querySelectorAll('.card');
+    const numberCart = document.querySelector('.number-cart');
+    numberCart.textContent = cartItems.length;
+}
